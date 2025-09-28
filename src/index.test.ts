f9e7ade1756f04a1bbafb379f6f1a0025c8cdcd9
Darren B <68653294+Devd0@users.noreply.github.com>
2025-09-27 21:55:14 -0400

    remove tests

--- src/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { helloWorld } from './index';
-import { test, expect } from 'vitest';
-
-test('should pass', () => {
-	expect(helloWorld()).toBe('helloWorld');
-});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export function helloWorld() {
-	const thing = 1;
-	return 'helloWorld';
-}
-
-// Export all icons
 export * from './icons';
